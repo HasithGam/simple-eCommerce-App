549af745f7a28b6c088dde7cbec47447aa76004d
HasithGam <159763235+HasithGam@users.noreply.github.com>
2024-05-04 15:00:20 +1000

    040520240300pm

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// index.js - updated version
+// import the app
+const app = require('./app');
+const port = 3000;
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
+// start the app to listen on the right port
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
