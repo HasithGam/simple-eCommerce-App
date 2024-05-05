68c96b687206b9a1bbe89056aa1fdb6104f9649d
HasithGam <159763235+HasithGam@users.noreply.github.com>
2024-05-05 10:39:02 +1000

    050520241038am

--- server/app.js
+++ server/app.js
@@ -1,10 +1,5 @@
-// app.js - new file at top level
-const express = require('express')
-const app = express()
-const port = 3000
-// map all routes to the express app
-// const calculatorRoutes =
-//     require('./routes/calculatorRoutes');
-// app.use('/calculator', calculatorRoutes);
-// export the app
-module.exports = app;
+// // app.js - new file at top level
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// module.exports = app;
